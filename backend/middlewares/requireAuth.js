3fd02d5dbe88674bc6c1a0d22c49cb320d607fc5
Bhargav Mule <bhargavmule16@gmail.com>
2025-03-07 17:04:10 +0530

    next function was missing , added next function

--- backend/middlewares/requireAuth.js
+++ backend/middlewares/requireAuth.js
@@ -10,10 +10,13 @@ const requireAuth = async (req, res, next) => {
 
 	const token = authorization.split(" ")[1]
 
+
 	try {
 		const { _id } = jwt.verify(token, process.env.SECRET)
 
 		req.user = await User.findOne({ _id }).select("_id")
+
+		next()
 
 	} catch (error) {
 		res.status(400).json({ error: "Request is not authorized" })
