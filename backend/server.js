7bf4bc6e2ac062222b4a04191fe6b78a2389ac99
Bhargav Mule <bhargavmule16@gmail.com>
2025-03-07 16:54:11 +0530

    cors was missing in backend causing issue , added cors policy

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const helmet = require("helmet")
 const compression = require("compression")
 const workoutRoutes = require("./routes/workouts")
 const userRoutes = require("./routes/user")
+const cors = require("cors")
 
 const app = express()
 
@@ -13,6 +14,9 @@ const app = express()
 app.use(helmet())
 app.use(compression())
 app.use(express.json())
+app.use(cors({
+	origin:process.env.URL
+}))
 
 app.use((req, res, next) => {
 	console.log(req.path, req.method)
